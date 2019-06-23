a0659957b527d8b0a74dd93d5cc940b86acb3e4a
abhirup-deb <debabhirup1999@gmail.com>
2019-06-23 20:30:21 +0530

    3rd commit

--- bot.py
+++ bot.py
@@ -15,7 +15,10 @@ class InstagramBot :
     def login(self) :
         self.driver.get('https://www.instagram.com/accounts/login/')
 
+        self.driver.find_element_by_name('username').send_keys(self.username)
+        self.driver.find_element_by_name('password').send_keys(self.password)
 
+        self.driver.find_elements_by_xpath("//div[contains(text(), 'Log In')]")[0]
 
 
 if __name__ == '__main__' :
