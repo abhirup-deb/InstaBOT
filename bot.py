89745371cbae0d27478f9af3c25bf0730dc64337
abhirup-deb <debabhirup1999@gmail.com>
2019-06-23 11:24:49 +0530

    2nd commit

--- bot.py
+++ bot.py
@@ -1,3 +1,24 @@
 from selenium import webdriver
 import os
 import time
+
+class InstagramBot :
+
+    def __init__(self,username,password) :
+        self.username = username
+        self.password = password
+
+        self.driver = webdriver.Chrome('./chromedriver.exe')
+
+        self.login()
+
+    def login(self) :
+        self.driver.get('https://www.instagram.com/accounts/login/')
+
+
+
+
+if __name__ == '__main__' :
+    ig_bot = InstagramBot('temp_username','temp_password')
+
+    print(ig_bot.username)
